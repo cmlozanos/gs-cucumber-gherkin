b91f646ed738ac65f45f3cceb5341a5433dd147d
Carlos Lozano <carlosmlozanos@gmail.com>
2020-07-17 10:20:38 +0200

    Init push

---
+++ src/main/java/cmls/spring/gscucumbergherkin/GsCucumberGherkinApplication.java
@@ -0,0 +1,13 @@
+package cmls.spring.gscucumbergherkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsCucumberGherkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsCucumberGherkinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cmls/spring/gscucumbergherkin/GsCucumberGherkinApplicationTests.java
@@ -0,0 +1,13 @@
+package cmls.spring.gscucumbergherkin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsCucumberGherkinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
